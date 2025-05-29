54dff8e668a4f50f5bf0edc5b175a60b1aae1795
Bill Liu <billyliu1221@gmail.com>
2025-05-29 11:31:43 -0700

    edit: datastore and player classes

--- src/DataStore.java
+++ src/DataStore.java
@@ -1,2 +1,38 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public class DataStore {
+    private static final String FILE_NAME = "players.txt";
+
+    public static void savePlayers(ArrayList<Player> players) {
+        try {
+            FileOutputStream fos = new FileOutputStream(FILE_NAME);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(players);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Player> loadPlayers() {
+        File file = new File(FILE_NAME);
+        if (!file.exists()) {
+            return new java.util.ArrayList<>();
+        }
+
+        try {
+            FileInputStream fis = new FileInputStream(FILE_NAME);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            ArrayList<Player> players = (ArrayList<Player>) ois.readObject();
+            ois.close();
+            fis.close();
+            return players;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return new java.util.ArrayList<>();
+        }
+    }
+
 }
--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,116 @@
-public class Player {
-    private String name;
-    private int score;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
+public class Player implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L; // to identify the class even if fields/methods change
+
+    // instance variables
+    private String username;
+    private String name;
+    private String email;
+    private String password;
+
+    /**
+     * Constructor(s)
+     */
+
+    public Player() {
+        // default constructor needed for deserialization
+    }
+
+    // do not set up the accounts here yet
+    public Player(String username, String name, String email, String password) {
+        if (username == null || username.isBlank()) {
+            throw new IllegalArgumentException("A username is required.");
+        }
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("A name is required.");
+        }
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("An email is required.");
+        }
+        if (password == null || password.isBlank()) {
+            throw new IllegalArgumentException("A password is required,");
+        }
+        this.username = username;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    // mutators and accessors
+    // all validation done in main class
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String newUsername) {
+        if (newUsername == null || newUsername.isBlank()) {
+            throw new IllegalArgumentException("New username cannot be blank.");
+        }
+        this.username = newUsername;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        if (newName == null || newName.isBlank()) {
+            throw new IllegalArgumentException("New name cannot be blank.");
+        }
+        this.name = newName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String newEmail) {
+        if (newEmail == null || newEmail.isBlank()) {
+            throw new IllegalArgumentException("New email cannot be blank.");
+        }
+        this.email = newEmail;
+    }
+
+    protected String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String newPassword) {
+        if (newPassword == null || newPassword.isBlank()) {
+            throw new IllegalArgumentException("New password cannot be blank.");
+        }
+        this.password = newPassword;
+    }
+
+    /**
+     * Authentication
+     */
+    public boolean checkPassword(String inputPassword) {
+        return getPassword().equals(inputPassword);
+    }
+
+    public String getObfuscatedPassword() {
+        System.out.println("Your password has been obfuscated.");
+        String obfuscatedPassword = "";
+        for (int i = 0; i < password.length(); i++) {
+            obfuscatedPassword += "*";
+        }
+        return obfuscatedPassword;
+    }
+
+    /**
+     * Output/format player info
+     */
+    @Override
+    public String toString() {
+        return String.format("Username: %s\nName: %s\nEmail: %s\nObfuscated Password: %s\nDefault Account: %s",
+                getUsername(), getName(), getEmail(), getObfuscatedPassword(), getDefaultAccount().getAccountNum());
+    }
 }
